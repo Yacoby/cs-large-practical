554ab7ce5507661dc1b862e95dd08a5cd247044b
Jacob Essex <jacob@jacobessex.com>
2012-09-28 09:24:49 +0100

    - Added some requirements for parsing reactions

--- tests/ConfigurationSerilizerTest.m
+++ tests/ConfigurationSerilizerTest.m
@@ -22,6 +22,29 @@ void testDeserilize_WhenHasTimeAndKineticConstant_ParsesConstantCorrectly(){
     PASS_INT_EQUAL([[[ConfigurationTextSerilizer deserilize:configurationStr] kineticConstant:@"d"] doubleValue], 5, "");
 }
 
+void testDeserilize_WhenHasBasicReaction_ParsesReactionCorrectly(){
+    NSString* configurationStr = @"p = 1\np : F -> A";
+    SimulationConfiguration* cfg = [ConfigurationTextSerilizer deserilize:configurationStr];
+    ReactionDefinition* reaction = [cfg reaction:@"p"];
+
+    PASS_INT_EQUAL([[reaction requirements] countForObject:@"F"], 1, "");
+    PASS_INT_EQUAL([[reaction requirements] count], 1, "There is only one requirement for the reaction");
+
+    PASS_INT_EQUAL([[reaction result] countForObject:@"A"], 1, "The result of the reaction should be A");
+    PASS_INT_EQUAL([[reaction result] count], 1, "There should be only one result from the reaction");
+}
+
+void testDeserilize_WhenHasDestructiveReaction_ParsesReactionCorrectly(){
+    NSString* configurationStr = @"p = 1\np : F ->";
+    SimulationConfiguration* cfg = [ConfigurationTextSerilizer deserilize:configurationStr];
+    ReactionDef* reaction = [cfg reaction:@"p"];
+    PASS_INT_EQUAL([[reaction requirements] countForObject:@"F"], 1, "");
+    PASS_INT_EQUAL([[reaction requirements] count], 1, "");
+
+    PASS_INT_EQUAL([[reaction result] count], 0, "There should be no results of the reaction");
+}
+
+
 int main()
 {
     START_SET("ConfigurationTxtSerilizer")

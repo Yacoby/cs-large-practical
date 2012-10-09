b84f1c14c6e28b9cc071cba3aaea695b186cec3b
Jacob Essex <jacob@jacobessex.com>
2012-10-09 17:30:05 +0100

    - Counted allocations of objects

--- Main.m
+++ Main.m
@@ -1,10 +1,32 @@
 #import <Foundation/Foundation.h>
 #import "ConfigurationSerilizer.h"
 
+void countAllocationsForAllClasses(){
+    int numClasses;
+    numClasses = objc_getClassList(NULL, 0);
+    if (numClasses > 0) {
+        Class* classes = malloc(sizeof(Class) * numClasses);
+        (void) objc_getClassList (classes, numClasses);
+        int i;
+        for (i = 0; i < numClasses; i++) {
+            GSDebugAllocationActiveRecordingObjects(classes[i]);
+        }
+        free(classes);
+    }
+}
+
+void printAllocatedClasses(){
+    printf(GSDebugAllocationList(false));
+}
+
 int main(void){
     NSAutoreleasePool *pool = [[NSAutoreleasePool alloc] init];
+
+    countAllocationsForAllClasses();
+
     NSString* configurationStr = @"t = 10\n";
-    NSLog(@"%d",[[ConfigurationTextSerilizer deserilize:configurationStr] time]);
+    NSLog(@"%f",[[[ConfigurationTextSerilizer deserilize:configurationStr] time] totalSeconds]);
     [pool drain];
+
     return 0;
 }
